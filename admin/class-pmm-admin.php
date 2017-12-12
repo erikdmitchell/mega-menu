b167571ba3977c4561f784563c73fd6340dd017b
Erik <erikdmitchell@gmail.com>
2017-12-12 11:11:12 -0500

    built out saving menu functions

--- admin/class-pmm-admin.php
+++ admin/class-pmm-admin.php
@@ -49,24 +49,103 @@ class PMM_Admin {
     public function save_menu() {
         if (!isset($_POST['pmm_admin']) || !wp_verify_nonce($_POST['pmm_admin'], 'pmm_save_menu'))
             return;
+
+        $this->update_menu($_POST['menu_name'], $_POST['menu_id']);
+    }
+    
+    private function update_menu($menu_name='', $menu_id=0) {
+       // Add new menu.
+       if (0 == $menu_id) :
+            $new_menu_title = trim(esc_html($menu_name));
             
+            if ($new_menu_title) :
+ 
+				$_nav_menu_selected_id = wp_update_nav_menu_object( 0, array('menu-name' => $new_menu_title) );
+
+				if ( is_wp_error( $_nav_menu_selected_id ) ) {
+					//$messages[] = '<div id="message" class="error notice is-dismissible"><p>' . $_nav_menu_selected_id->get_error_message() . '</p></div>';
+					// message about error
+				} else {
+					$_menu_object = wp_get_nav_menu_object( $_nav_menu_selected_id );
+					$nav_menu_selected_id = $_nav_menu_selected_id;
+					$nav_menu_selected_title = $_menu_object->name;
+					
+					// Save menu items.
+					//if ( isset( $_REQUEST['menu-item'] ) )
+						//wp_save_nav_menu_items( $nav_menu_selected_id, absint( $_REQUEST['menu-item'] ) );
+						
 /*
-        wp_terms
-            term_id **
-            name ($_POST['menu_name'])
-            slug (sanitize_title_with_dashes($_POST['menu_name']))
-            term_group
-            
-        wp_term_taxonomy
-            term_taxonomy_id
-            term_id **
-            taxonomy nav_menu
-            description
-            parent
-            count
-            
-        generates a custom post type 'nav_menu_item'
+					if ( isset( $_REQUEST['zero-menu-state'] ) ) {
+						// If there are menu items, add them
+						wp_nav_menu_update_menu_items( $nav_menu_selected_id, $nav_menu_selected_title );
+						// Auto-save nav_menu_locations
+						$locations = get_nav_menu_locations();
+						foreach ( $locations as $location => $menu_id ) {
+								$locations[ $location ] = $nav_menu_selected_id;
+								break; // There should only be 1
+						}
+						set_theme_mod( 'nav_menu_locations', $locations );
+					}
 */
+					
+/*
+					if ( isset( $_REQUEST['use-location'] ) ) {
+						$locations = get_registered_nav_menus();
+						$menu_locations = get_nav_menu_locations();
+						if ( isset( $locations[ $_REQUEST['use-location'] ] ) )
+							$menu_locations[ $_REQUEST['use-location'] ] = $nav_menu_selected_id;
+						set_theme_mod( 'nav_menu_locations', $menu_locations );
+					}
+*/
+
+					// $messages[] = '<div id="message" class="updated"><p>' . sprintf( __( '<strong>%s</strong> has been created.' ), $nav_menu_selected_title ) . '</p></div>';
+					wp_redirect( admin_url( 'themes.php?page=pickle-mega-menu&menu=' . $_nav_menu_selected_id ) );
+					exit();
+					
+					// REDIRECT ???
+				} 
+            
+            else :
+                // message about error
+            endif;
+       
+       // Update existing menu.
+       else :
+            $_menu_object = wp_get_nav_menu_object( $menu_id );
+
+			$menu_title = trim( esc_html( $_POST['menu-name'] ) );
+			
+			if ( ! $menu_title ) {
+				$messages[] = '<div id="message" class="error notice is-dismissible"><p>' . __( 'Please enter a valid menu name.' ) . '</p></div>';
+				$menu_title = $_menu_object->name;
+			}
+
+            // Update menut object.
+			if ( ! is_wp_error( $_menu_object ) ) {
+				$_nav_menu_selected_id = wp_update_nav_menu_object( $nav_menu_selected_id, array( 'menu-name' => $menu_title ) );
+				if ( is_wp_error( $_nav_menu_selected_id ) ) {
+					$_menu_object = $_nav_menu_selected_id;
+					$messages[] = '<div id="message" class="error notice is-dismissible"><p>' . $_nav_menu_selected_id->get_error_message() . '</p></div>';
+				} else {
+					$_menu_object = wp_get_nav_menu_object( $_nav_menu_selected_id );
+					$nav_menu_selected_title = $_menu_object->name;
+				}
+			}
+
+			// Update menu items.
+/*
+			if ( ! is_wp_error( $_menu_object ) ) {
+				$messages = array_merge( $messages, wp_nav_menu_update_menu_items( $_nav_menu_selected_id, $nav_menu_selected_title ) );
+
+				// If the menu ID changed, redirect to the new URL.
+				if ( $nav_menu_selected_id != $_nav_menu_selected_id ) {
+					wp_redirect( admin_url( 'nav-menus.php?menu=' . intval( $_nav_menu_selected_id ) ) );
+					exit();
+				}
+			} 
+*/      
+       endif;
+        
     }
     
 }
--- admin/pages/main.php
+++ admin/pages/main.php
@@ -1,4 +1,12 @@
 <?php
+// The menu id of the current menu being edited
+$nav_menu_selected_id = isset( $_REQUEST['menu'] ) ? (int) $_REQUEST['menu'] : 0;
+
+// Nav menu object
+$nav_menu_object = wp_get_nav_menu_object($nav_menu_selected_id);
+
+//print_r($nav_menu_object);
+    
 // Get existing menu locations assignments
 $locations = get_registered_nav_menus();
 $menu_locations = get_nav_menu_locations();
@@ -65,23 +73,23 @@ $num_locations = count( array_keys( $locations ) );
     
     <div class="menu-management-column">
         
-       
+        <form name="save-menu" id="save-menu" action="" method="post">       
             
             <div class="menu-management">
                 
                 <h3>Menu</h3>
                 
-                <form name="save-menu" id="save-menu" action="" method="post">
+                
                     <?php wp_nonce_field('pmm_save_menu', 'pmm_admin'); ?>
                     
                     <label for="menu-name">Menu Name</label>
-                    <input type="text" name="menu_name" id="menu-name" value="" />
+                    <input type="text" name="menu_name" id="menu-name" placeholder="Menu Name" value="<?php echo $nav_menu_object->name; ?>" />
                     
                      <span class="save-menu-button">
                         <input type="submit" class="button button-primary" value="Save Menu">
                     </span> 
                     
-                </form>
+                
                 
                 <div id="pmm-menu-grid">
                     gRID
@@ -89,7 +97,9 @@ $num_locations = count( array_keys( $locations ) );
                 
             </div>
             
-        
+            <input type="hidden" name="menu_id" id="menu-id" value="<?php echo $nav_menu_selected_id; ?>" />
+            
+        </form>
         
     </div>
     
