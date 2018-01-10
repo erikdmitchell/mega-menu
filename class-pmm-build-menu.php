c6535fd3dd0887a98fdca171d83a87a12695212c
Erik <erikdmitchell@gmail.com>
2018-01-10 11:25:37 -0500

    building out menu into list form, adding submenus

--- class-pmm-build-menu.php
+++ class-pmm-build-menu.php
@@ -19,11 +19,91 @@ class PMM_Build_Menu {
         $this->menu_object_id = $menu_object->term_id;
         $this->menu_items = wp_get_nav_menu_items($this->menu_object_id);
     }
+/*
+mega-menu
+
+    mega-menu-item mega-menu-item-SLUG id: mega-menu-item-POST mega-menu-item-has-children
     
+        mega-menu-link
+        
+        mega-sub-menu
+        
+            mega-menu-item
+                mega-menu-link
+                
+                mega-menu-row (li)
+                mega-sub-menu (ul)
+                
+                    mega-menu-column (li)
+                    
+                        mega-sub-menu (ul) 
+*/    
     public function display() {
-        return $this->build_menu();
+        //return $this->build_menu();
+        
+        /*
+          primary nav, does it have sub, then add  
+        */
+        
+        $html='';        
+        
+        if (empty($this->menu_items))
+            return;
+            
+        $primary_nav_items = $this->get_primary_nav_items();
+        
+        $html.='<ul class="pmm-mega-menu">';
+        
+        foreach ($primary_nav_items as $primary_nav_item) :
+            $classes = array("pmm-mega-menu-item", "pmm-mega-menu-item-{$primary_nav_item->post_name}", "pmm-mega-menu-item-{$primary_nav_item->ID}");
+            
+            if ($this->has_subnav($primary_nav_item->pmm_order))
+                $classes[] = 'pmm-mega-menu-item-has-children';
+//print_r($primary_nav_item);        
+
+            $html.='<li id="pmm-mega-menu-item-'.$primary_nav_item->ID.'" class="'.implode(' ', $classes).'">';
+                $html.='<a class="pmm-mega-menu-link" href="'.get_permalink($primary_nav_item->ID).'">'.$primary_nav_item->title.'</a>';
+                
+                    if ($this->has_subnav($primary_nav_item->pmm_order))
+                        $html.=$this->get_subnav($primary_nav_item->pmm_order);
+                
+            $html.='</li>';            
+        endforeach;
+        
+        $html.='</ul>';
+        
+        return $html;
+        
+    }
+    
+    protected function get_primary_nav_items() {
+        $primary_nav_items = array();
+        
+        // pul out primary nav items.
+        foreach ($this->menu_items as $menu_item) :
+            if ('primary' === $menu_item->pmm_nav_type)
+                $primary_nav_items[] = $menu_item;
+        endforeach;
+        
+        // sort by order.
+        usort($primary_nav_items, function($a, $b) {
+           return $a->pmm_order - $b->pmm_order; 
+        });
+        
+        return $primary_nav_items;       
+    }
+    
+    protected function has_subnav($sub_nav_id = 0) {
+        foreach ($this->menu_items as $menu_item) :       
+            if ($menu_item->pmm_nav_type == 'subnav' && $menu_item->pmm_menu_primary_nav === $sub_nav_id) :
+                return true;
+            endif;
+        endforeach;
+        
+        return false;       
     }
 
+/*
     protected function build_menu() {
         $html='';
         $layout = $this->get_columns_and_rows();
@@ -40,50 +120,25 @@ class PMM_Build_Menu {
 
         return $html;
     }
+*/
 
-    public function build_primary_nav() {
-        $html='';        
-        $primary_nav_items = array();
-        
-        if (empty($this->menu_items))
-            return $primary_nav_items;
-        
-        // pul out primary nav items.
-        foreach ($this->menu_items as $menu_item) :
-            if ('primary' === $menu_item->pmm_nav_type)
-                $primary_nav_items[] = $menu_item;
-        endforeach;
-        
-        // sort by order.
-        usort($primary_nav_items, function($a, $b) {
-           return $a->pmm_order - $b->pmm_order; 
-        });
-            
-        // get our items.
-        foreach ($primary_nav_items as $primary_nav_item) :
-            if (isset($primary_nav_item->pmm_item_type) && '' !== $primary_nav_item->pmm_item_type)
-                $html.='<div class="pmm-item pmm-primary-nav-item"><a href="'.get_permalink($primary_nav_item->ID).'">'.$primary_nav_item->title.'</a></div>';            
-        endforeach;
-        
-        return $html;
-    }
-
-/*
     public function get_subnav($sub_nav_id = 0) {       
         $html='';
         $sub_menu_items = $this->get_sub_nav_items($sub_nav_id);
         $layout = $this->get_columns_and_rows($sub_menu_items);
-
-        foreach ($layout as $column => $blocks) :
-            $html.=$this->add_column($column, $blocks, $sub_menu_items);
-        endforeach;
+        
+        $html.='<ul class="pmm-mega-menu-sub">';
+        
+            foreach ($layout as $column => $blocks) :
+                $html.=$this->add_column($column, $blocks, $sub_menu_items);
+            endforeach;
+        
+        $html.='</ul>';
 
         return $html;
 
     }
-*/
     
-/*
     protected function get_sub_nav_items($sub_nav_id = 0) {
         $sub_menu_items = array();
            
@@ -95,7 +150,6 @@ class PMM_Build_Menu {
         
         return $sub_menu_items;
     }
-*/
     
     protected function get_columns_and_rows() {
         $columns_and_rows = array();
