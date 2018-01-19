c613213347760a3ec4ac215ea6db6707fe4990cf
Erik <erikdmitchell@gmail.com>
2018-01-19 10:51:32 -0500

    custom navigation label works

--- admin/class-pmm-admin-save-menu.php
+++ admin/class-pmm-admin-save-menu.php
@@ -45,7 +45,7 @@ class PMM_Admin_Save_Menu {
                     $form_submenu_items[] = $form_menu_item; 
             endforeach;
 print_r($form_submenu_items);     
-            //echo $this->update_submenu_nav_menu_items($form_data['menu_id'], $form_data['menu_name'], $_POST['sub_nav_id'], $form_submenu_items);
+            echo $this->update_submenu_nav_menu_items($form_data['menu_id'], $form_data['menu_name'], $_POST['sub_nav_id'], $form_submenu_items);
         else :
 // no items        
         endif;
